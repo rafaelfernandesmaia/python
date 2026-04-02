b44c0e449c4027050424a4765ad93110e21abc51
rafaelfernandesmaia <rafaelmaia1238@gmail.com>
2026-04-02 17:17:10 -0300

    att

---
+++ exercicios/aula07.py
@@ -0,0 +1,45 @@
+('''for c in range(1, 6):
+        print('Oi')
+    print('Fim') ''')
+
+('''for c in range (1, 6):
+        print('Oi')
+        print('Fim')''')
+
+('''for c in range (1, 6):
+        print(c)
+    print('Fim')''')
+
+('''for c in range (6, 0, -1):
+        print(c)
+    print('Fim')''')
+
+('''for c in range(0, 6, 2):
+        print(c)
+    print('Fim')''')
+
+('''n = int(input('Digite um número: '))
+    for c in range(0, n+1):
+        print(c)
+    print('Fim')''')
+
+('''i = int(input('Inicio: '))
+    f = int(input('Fim: '))
+    p = int(input('Passo: '))
+    for c in range(i, f+1, p):
+        print(c)
+    print('Fim')''')
+
+('''for c in range(0, 3):
+        n = int(input('Digite um valor: '))
+    print('Fim')''')
+
+('''s = 0
+    for c in range(0, 6):
+        n = int(input('Digite um valor: '))
+        s = s + n // s += n
+    print(f'O somatorio de todos os valores foi {s})''')
+
+
+
+
