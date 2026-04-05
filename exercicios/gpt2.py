582536d20d0c24d454890783d08fd96b06415c8f
rafaelfernandesmaia <263433995+rafaelfernandesmaia@users.noreply.github.com>
2026-04-05 20:12:40 -0300

    att

---
+++ exercicios/gpt2.py
@@ -0,0 +1,29 @@
+# Contagem simples
+'''contador = 0
+while contador <= 10:
+    print(contador)
+    contador += 1'''
+
+# Contagem regressiva
+'''contador = 10
+while contador >= 0:
+    print(contador)
+    contador -= 1'''
+
+# Somando números
+'''soma = 0
+contador = 1
+while contador <= 5:
+    num = int(input('Digite um valor: '))
+    soma += num
+    contador += 1
+print(f'A soma entre os 5 números será {soma}')'''
+
+# Tabuada
+'''contador = 1
+num = int(input('Digite um valor: '))
+while contador <= 10:
+    total = num * contador
+    print(f'{num} x {contador} = {total}')
+    contador += 1
+print('==' * 11)'''
