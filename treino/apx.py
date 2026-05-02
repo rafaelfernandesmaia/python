ff810135fe1cef2548c62db61550b9e0df93c560
rafaelfernandesmaia <263433995+rafaelfernandesmaia@users.noreply.github.com>
2026-05-01 22:18:06 -0300

    att

---
+++ treino/apx.py
@@ -0,0 +1,16 @@
+# LÓGICA DE PROGRAMAÇÃO EM PYTHON + TREINO/EXERCÍCIOS
+# NÍVEL INICIANTE
+'''
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+s = n1 + n2
+print(f'A soma de {n1} e {n2} é igual a {s}')
+'''
+
+'''
+num = int(input("Digite um valor: "))
+if num % 2 == 0:
+    print("Par")
+else:
+    print("Impar")
+'''
