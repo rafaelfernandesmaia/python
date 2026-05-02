1db302e384e238ac29aa95076fc41a83cc1fe75a
rafaelfernandesmaia <rafaelmaia1238@gmail.com>
2026-05-02 17:15:02 -0300

    Update preparo.py

--- treino/preparo.py
+++ treino/preparo.py
@@ -1,3 +1,35 @@
+# EXERCÍCIO 01
 '''
 print("Olá, Mundo!")
 '''
+
+# EXERCÍCIO 02
+''' 
+nome = str(input("Qual é o seu nome? "))
+print(f'Muito prazer em te conhecer {nome}!')
+'''
+
+# EXERCÍCIO 03
+'''
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+print(f'A soma entre {n1} e {n2} é igual a {s}')
+'''
+
+# EXERCÍCIO 04
+'''
+frase = input('Digite algo: ')
+print('O tipo primitivo desse valor é ', type(frase))
+print('Só tem espaços?', frase.isspace())
+print('É um número?', frase.isnumeric)
+print('É alfabético?', frase.isalpha)
+print('É alfanúmerico?', frase.isalnum)
+print('Está em maiúsculas', frase.isupper)
+print('Está em minúsculas', frase.islower)
+print('Está capitalizada?', frase.istitle)
+'''
+
+# EXERCÍCIO 05
+num = int(input('Digite um número: '))
+print(f'Analisando o número {num}, seu antecessor é {num-1} e o seu sucessor é {num+1}')
