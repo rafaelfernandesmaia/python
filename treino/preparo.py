8cefb6d58869fed8972a0991570a3d18aa9c2583
rafaelfernandesmaia <263433995+rafaelfernandesmaia@users.noreply.github.com>
2026-05-10 14:09:03 -0300

    novo

--- treino/preparo.py
+++ treino/preparo.py
@@ -178,3 +178,17 @@ print(f'Seu nome em minúsculas é {(nome.lower())}')
 print(f'Seu nome ao todo {(len(nome) - nome.count(' '))} letras')
 print(f'Seu primeiro nome tem {(nome.find(' '))} letras')
 '''
+
+# EXERCÍCIO 23
+'''
+num = int(input('Informe um número: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print(f'Analisando o número {num}')
+print(f'Unidade: {u}')
+print(f'Dentena: {d}')
+print(f'Centena: {c}')
+print(f'Milhar: {m}')
+'''
