d4df2a313a297b7dd6f3e6b5aad4ce35e326d73e
rafaelfernandesmaia <263433995+rafaelfernandesmaia@users.noreply.github.com>
2026-05-10 14:23:52 -0300

    up

--- treino/preparo.py
+++ treino/preparo.py
@@ -192,3 +192,18 @@ print(f'Dentena: {d}')
 print(f'Centena: {c}')
 print(f'Milhar: {m}')
 '''
+
+# EXERCÍCIO 28
+from random import randint
+from time import sleep
+computador = randint(0, 5) # Faz o computador "PENSAR"
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=-' * 20)
+jogador = int(input('Em que número eu pensei? ')) # Jogador tenta adivinhar
+print('PROCESSANDO...')
+sleep(3)
+if jogador == computador:
+    print('PARABÉNS! Você conseguiu me vencer!')
+else:
+    print(f'GANHEI! EU pensei no número {computador} e não no {jogador}')
