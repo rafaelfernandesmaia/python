eaeb40943c5f40239f3c8e05d7ee12f0b7199186
Rafael Maia <rafaelmaia1238@gmail.com>
2026-06-19 11:01:32 -0300

    py

--- trainingpython/upgrade.py
+++ trainingpython/upgrade.py
@@ -1,3 +1,21 @@
-# Python - Curso em Video Exercício
+# Python - Curso em Video Exercícios
 
 # 001
+'''
+print("Olá, Mundo!")
+print("Me livrei da maldição")
+'''
+
+# 002
+'''
+name = input("Qual é o seu nome: ")
+print(f"É um prazer te conhecer {name}")
+'''
+
+# 003
+'''
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+s = n1 + n2
+print(f"A soma entre {n1} e {n2} é igual a {s}")
+'''
