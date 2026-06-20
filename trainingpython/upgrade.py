49e464ab6f4fd9b78d419d651de6b4b808a1da52
Rafael Maia <rafaelmaia1238@gmail.com>
2026-06-20 19:41:15 -0300

    nv

--- trainingpython/upgrade.py
+++ trainingpython/upgrade.py
@@ -147,4 +147,30 @@ print(f"O ângulo de {ang} tem o TANGENTE de {tan:.2f}")
 '''
 
 # 019
-import math
+'''
+import random
+a1 = str(input("Primeiro aluno: "))
+a2 = str(input("Segundo aluno: "))
+a3 = str(input("Terceiro aluno: "))
+a4 = str(input("Quarto aluno: "))
+lista = [a1, a2, a3, a4]
+escolhido = random.choice(lista)
+print(f"O aluno escolhido foi {escolhido}")
+'''
+
+# 020
+'''
+import random
+a1 = str(input("Primeiro aluno: "))
+a2 = str(input("Segundo aluno: "))
+a3 = str(input("Terceiro aluno: "))
+a4 = str(input("Quarto aluno: "))
+lista = [a1, a2, a3, a4]
+random.shuffle(lista)
+print("A ordem da apresentação será ")
+print(lista)
+'''
+
+# 021
+# Tocando MP3
+
