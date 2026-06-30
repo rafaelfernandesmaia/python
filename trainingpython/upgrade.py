ab0525d6b69863e754a07e234e383e653d49edfa
Rafael Maia <rafaelmaia1238@gmail.com>
2026-06-30 20:31:17 -0300

    up

--- trainingpython/upgrade.py
+++ trainingpython/upgrade.py
@@ -1084,3 +1084,24 @@ else:
     print('Sua expressão não é válida')
 '''
 
+# 084
+temp = []
+princ = []
+mai = men = 0
+while True:
+    temp.append(str(input('Nome: ')))
+    temp.append(float(input('Peso: ')))
+    if len(princ) == 0:
+        mai = men = temp[1]
+    else:
+        if temp[1] > mai:
+            mai = temp[1]
+        if temp[1] < men:
+            men = temp[1]
+    princ.append(temp[:])
+    resp = str(input("Quer continuar [S/N]: "))
+    if resp in 'Nn':
+        break
+print("-=" * 15)
+print(f"Os dados foram {princ}")
+print(f"Ao todo, você cadastrou {len(princ)} pessoas")
