5e128fc03367f341eb2d6072a9dff3d392f42c4c
Rafael Maia <rafaelmaia1238@gmail.com>
2026-07-02 11:15:57 -0300

    up

--- trainingpython/upgrade.py
+++ trainingpython/upgrade.py
@@ -1281,3 +1281,57 @@ nome = str(input("Digite o nome do jogador: "))
 gols = int(input("Digite a quantidade de gols: "))
 ficha(nome, gols)
 '''
+
+# 104
+'''
+# Função
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[0;031mERRO! Digite um número inteiro válido.\033[m')
+        if ok:
+            break
+    return valor
+
+
+
+# Programa principal
+n = leiaInt('Digite um número: ')
+print(f"Você acabou de digitar o número {n}")
+'''
+
+# 105
+'''
+def notas(*num, sit=False):
+    cont = maior = menor = media = total = 0
+    for c in num:
+        cont += 1
+        if c > maior:
+            maior = c
+        if c < menor or cont == 1:
+            menor = c
+        total += c
+    media = total / cont
+    if sit:
+        print('A situação da turma é: ', end='')
+        if media > 7:
+            print('Boa!')
+        elif media > 5:
+            print('Rasoável')
+        else:
+            print('Ruim!')
+
+    return print(f'A nota total da turma é {total}, maior nota é {maior}, a menor nota é {menor} e a media da turma é {media}')
+
+
+resp = notas(8, 6, 5, 9, 6, 9, sit=True)
+print(resp)
+'''
+
+# 106
