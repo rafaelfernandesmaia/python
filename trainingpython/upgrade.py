b60d0e54784d97f4ff85294be5b02ba2e8f3f74d
Rafael Maia <rafaelmaia1238@gmail.com>
2026-07-02 20:18:11 -0300

    up

--- trainingpython/upgrade.py
+++ trainingpython/upgrade.py
@@ -1334,4 +1334,49 @@ resp = notas(8, 6, 5, 9, 6, 9, sit=True)
 print(resp)
 '''
 
-# 106
+# 113
+'''
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mERRO: por favor, digite um número inteiro válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\033[31mUsuário preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
+        
+
+def leiaFloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mERRO: por favor, digite um número real válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\033[31mUsuário preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
+    
+n1 = leiaInt('Digite um número: ')
+n2 = leiaFloat('Digite um Real: ')
+print(f"O valor inteiro digitado foi {n1} e o real foi {n2}")
+'''
+
+# 114
+'''
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('https://www.pudim.com.br')
+except urllib.error.URLError:
+    print('O site Pudim não está acessível no momento')
+else:
+    print('Consegui acessar o site Pudim com sucesso!')
+'''
