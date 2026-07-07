2a4d6cd6006dfd6b9aa6925dc4fa0f35bd3f5365
Rafael Maia <rafaelmaia1238@gmail.com>
2026-07-07 12:53:55 -0300

    up

--- .projetospython/ListadeTarefas/listadetarefas.py
+++ .projetospython/ListadeTarefas/listadetarefas.py
@@ -1,5 +1,31 @@
 tarefas = []
 
+def mostrar_tarefa():
+    if not tarefas:
+        print('Nenhuma tarefa na lista')
+    else:
+        print('\nTarefas: ')
+        for i, tarefas in enumerate(tarefas, i):
+            status = '✓' if tarefas['concluída'] else '✗'
+            print(f'{i}. [{status}] {tarefas['descrição']}')
+
+def adicionar_tarefas():
+    desc = str(input('Digite uma descrição da tarefa: '))
+    tarefas.append({'descrição': {desc}, 'concluída': False})
+    print('Tarefa adicionada!')
+    
+
+def marcar_concluido():
+    mostrar_tarefa()
+    if tarefas:
+        n = int(input('Digite o número da tarefa que você concluiu: '))
+        if 1 <= n <= len(tarefas):
+            tarefas[n-1]['concluída'] = True
+            print('Tarefa marcada como concluída!')
+        else:
+            print('Número inválido!')
+
+
 while True:
     print('\nO que você deseja fazer?')
     print('1 - Mostrar tarefa')
@@ -7,15 +33,16 @@ while True:
     print('3 - Marcar tarefa como concluído')
     print('0 - Sair do programa')
 
-    escolha = int(input('Digite uma opção: ')).strip()[0]
+    escolha = input('Digite uma opção: ')
 
-    if escolha == 1:
+    if escolha == '1':
         mostrar_tarefa()
-    elif escolha == 2:
+    elif escolha == '2':
         adicionar_tarefa()
-    elif escolha == 3:
+    elif escolha == '3':
         marcar_concluido()
-    elif escolha == 0:
+    elif escolha == '0':
         print('Saindo...')
         break
     else:
+        print('Opção Inválida. Digite uma opção válida!')
