7409d2b879b6723f347c1a64b10f5ff6a10030dd
rafaelfernandesmaia <rafaelmaia1238@gmail.com>
2026-07-18 21:33:11 -0300

    Create main.py

---
+++ testandoclasses/main.py
@@ -0,0 +1,8 @@
+class Canal():
+    def __init__(self, nome, descricao, inscritos):
+        self.nome = nome
+        self.descricao = descricao
+        self.inscritos = inscritos
+
+canal_lancode = Canal('Lan Code', 'Códigos e gatos', 34000)
+print(canal_lancode)
